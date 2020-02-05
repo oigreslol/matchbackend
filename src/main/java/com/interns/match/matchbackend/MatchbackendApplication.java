b9dfd5fd867df3619dd042c89f7d30dc591bdde0
Sergio Rojas <sergiorojas12ocm@gmail.com>
2020-02-05 14:30:14 -0500

    Spring boot initial archetype

---
+++ src/main/java/com/interns/match/matchbackend/MatchbackendApplication.java
@@ -0,0 +1,13 @@
+package com.interns.match.matchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interns/match/matchbackend/MatchbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interns.match.matchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
